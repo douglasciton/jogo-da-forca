0ade85f5a728639c50ff2b7dbdcf3b8846767555
Douglas Vinícius Citon <125515152+douglasciton@users.noreply.github.com>
2023-07-05 11:06:08 -0300

    Add files via upload

---
+++ Projeto linguagem C/forca.h
@@ -0,0 +1,12 @@
+#define TAMANHO_PALAVRA 20
+
+void abertura();
+void chuta();
+int letraexiste(char letra);
+int jachutou(char letra);
+void desenhaforca();
+void escolhepalavra();
+int ganhou();
+int enforcou();
+void adicionapalavra();
+int chuteserrados();
